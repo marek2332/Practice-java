eb8dd81b64e0515fa2fbd429c78d943f88a6d9d3
marek2332 <markid2398@gmail.com>
2017-06-01 19:13:02 +0200

    Create PodatekDochodowy.java

---
+++ PodatekDochodowy/src/com/practise/podatekDochodowy/PodatekDochodowy.java
@@ -0,0 +1,41 @@
+/*
+ Zadanie 36 – PodatekDochodowy.java 
+ autor: Mark Danchyshyn 
+*/
+
+
+import java.util.Scanner;
+
+public class PodatekDochodowy{
+	
+	public static void main (String[] args) {
+		
+		// deklaracja zmiennej
+		double dochod;
+		
+		//wprowadzenia danych do konsoli
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Wpisz swoj dochod roczny: ");
+		dochod = sc.nextDouble();
+		
+		// wykorzystanie pentli if else
+		if(dochod <= 85528){
+		
+		//obliczanie podatku przy dochodzie ponizej 85528 zl		 	
+        double podatek = (dochod * 0.18) - 556.02;
+        
+        //wyswietlenia wynikow
+        System.out.print("Podatek Dochodowy = " + podatek + "zl");
+        
+		} else{
+			
+		//obliczanie podatku przy dochodzie powyzej 85528 zl	
+	    double podatek = (((dochod - 85528) * 0.32) + (85528 * 0.18));
+	    
+	    //wyswietlenia wynikow
+        System.out.print("Podatek Dochodowy = " + podatek + "zl");
+			
+		
+		}	
+    }
+}
